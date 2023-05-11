c931498acd6d6136e83f978dbf5c4fbd3c58eead
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-05-11 19:16:00 -0300

    feat: :sparkles: create new screens to tab bottom

---
+++ src/screens/Favorites/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+export function Favorites() {
+  return (
+    <View>
+      <Text>Favorites</Text>
+    </View>
+  );
+}
---
+++ src/screens/History/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+export function History() {
+  return (
+    <View>
+      <Text>History</Text>
+    </View>
+  );
+}
