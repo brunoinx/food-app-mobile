0696fb179e5ec564f586843c764383b16928540c
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-05-14 18:37:06 -0300

    feat: :sparkles: listening state auth user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import auth from '@react-native-firebase/auth';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
 import { TabRoutes } from './tab.routes';
 import { AuthStackRoutes } from './stacks/auth.routes';
 
-import { userStore } from '@/store';
+import { userStore } from '@/store/user.store';
 
 export function Routes() {
-  const { userIsValid } = userStore();
+  const { user, setUpdateUser } = userStore();
 
   const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
 
-  function onAuthStateChanged(user) {
-    setUser(user);
+  function onAuthStateChanged(_user: FirebaseAuthTypes.User) {
+    setUpdateUser({
+      id: _user.uid,
+      name: _user.displayName,
+      email: _user.email,
+      photoUrl: _user.photoURL,
+      phoneNumber: _user.phoneNumber,
+    });
+
     if (initializing) setInitializing(false);
   }
 
