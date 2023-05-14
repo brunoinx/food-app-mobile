6d5f7c443307fdf4445ecf234da96eab3d127425
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-05-14 18:33:16 -0300

    feat: :sparkles: add loading prop component button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { TouchableOpacityProps } from 'react-native';
+import { TouchableOpacityProps, ActivityIndicator } from 'react-native';
 
 import * as S from './styles';
+import { useTheme } from 'styled-components';
 
 export interface ButtonProps extends TouchableOpacityProps {
   name: string;
   type?: 'PRIMARY' | 'SECONDARY';
+  loading?: boolean;
 }
 
-export function Button({ name, type = 'PRIMARY', ...rest }: ButtonProps) {
+export function Button({
+  name,
+  type = 'PRIMARY',
+  loading,
+  ...rest
+}: ButtonProps) {
+  const { colors } = useTheme();
+
   return (
     <S.Container activeOpacity={0.8} type={type} {...rest}>
-      <S.Title type={type}>{name}</S.Title>
+      {loading ? (
+        <ActivityIndicator size="large" color={colors.white} />
+      ) : (
+        <S.Title type={type}>{name}</S.Title>
+      )}
     </S.Container>
   );
 }
