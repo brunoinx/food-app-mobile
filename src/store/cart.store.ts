270292813c01393b00c243dcf2349e618d1a6f9e
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-05-25 16:41:09 -0300

    refactor: :fire: remove and comment persist state

--- src/store/cart.store.ts
+++ src/store/cart.store.ts
@@ -1,7 +1,23 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+// import { MMKV } from 'react-native-mmkv';
+// import { persist, StateStorage } from 'zustand/middleware';
 
 import { FoodDTO } from '@/dtos/FoodDTO';
+
+// export const cartPersistStorage = new MMKV({ id: '@food-app/cart' });
+
+// const zustandMMKVStorage: StateStorage = {
+//   setItem: (name, value) => {
+//     return cartPersistStorage.set(name, value);
+//   },
+//   getItem: name => {
+//     const value = cartPersistStorage.getString(name);
+//     return value ?? null;
+//   },
+//   removeItem: name => {
+//     return cartPersistStorage.delete(name);
+//   },
+// };
 
 interface State {
   cart: FoodDTO[];
@@ -21,62 +37,57 @@ const INITIAL_STATE: State = {
   totalPrice: 0,
 };
 
-export const useCartStore = create(
-  persist<State & Actions>(
-    (set, get) => ({
-      cart: INITIAL_STATE.cart,
-      totalItems: INITIAL_STATE.totalItems,
-      totalPrice: INITIAL_STATE.totalPrice,
-      addToCart: food => {
-        const cart = get().cart;
-        const foodItem = cart.find(item => item.id === food.id);
+export const useCartStore = create<State & Actions>((set, get) => ({
+  cart: INITIAL_STATE.cart,
+  totalItems: INITIAL_STATE.totalItems,
+  totalPrice: INITIAL_STATE.totalPrice,
+  addToCart: food => {
+    const cart = get().cart;
+    const foodItem = cart.find(item => item.id === food.id);
 
-        // se o item já existir no carrinho, aumenta a quantidade
-        if (foodItem) {
-          const updatedCart = cart.map(
-            item => item.id === food.id && { ...item, amount: item.amount + 1 },
-          );
-          set(state => ({
-            cart: updatedCart,
-            totalItems: state.totalItems + 1,
-            totalPrice: state.totalPrice + food.value,
-          }));
-        } else {
-          const updatedCart = [...cart, { ...food, amount: 1 }];
+    // se o item já existir no carrinho, aumenta a quantidade
+    if (foodItem) {
+      const updatedCart = cart.map(
+        item => item.id === food.id && { ...item, amount: item.amount + 1 },
+      );
+      set(state => ({
+        cart: updatedCart,
+        totalItems: state.totalItems + 1,
+        totalPrice: state.totalPrice + food.value,
+      }));
+    } else {
+      const updatedCart = [...cart, { ...food, amount: 1 }];
 
-          set(state => ({
-            cart: updatedCart,
-            totalItems: state.totalItems + 1,
-            totalPrice: state.totalPrice + food.value,
-          }));
-        }
-      },
-      removeFromCart: food => {
-        set(state => ({
-          cart: state.cart.filter(item => item.id !== food.id),
-          totalItems: state.totalItems - 1,
-          totalPrice: state.totalPrice - food.value,
-        }));
-      },
-      decrementAmount: food => {
-        const cart = get().cart;
-        const foodItem = cart.find(item => item.id === food.id);
+      set(state => ({
+        cart: updatedCart,
+        totalItems: state.totalItems + 1,
+        totalPrice: state.totalPrice + food.value,
+      }));
+    }
+  },
+  removeFromCart: food => {
+    set(state => ({
+      cart: state.cart.filter(item => item.id !== food.id),
+      totalItems: state.totalItems - 1,
+      totalPrice: state.totalPrice - food.value,
+    }));
+  },
+  decrementAmount: food => {
+    const cart = get().cart;
+    const foodItem = cart.find(item => item.id === food.id);
 
-        if (foodItem.amount >= 2) {
-          const decrement = cart.map(
-            item => item.id === food.id && { ...item, amount: item.amount - 1 },
-          );
+    if (foodItem.amount >= 2) {
+      const decrement = cart.map(
+        item => item.id === food.id && { ...item, amount: item.amount - 1 },
+      );
 
-          set(state => ({
-            cart: decrement,
-            totalItems: state.totalItems - 1,
-            totalPrice: state.totalPrice - food.value,
-          }));
-        } else {
-          return;
-        }
-      },
-    }),
-    { name: 'food-storage' },
-  ),
-);
+      set(state => ({
+        cart: decrement,
+        totalItems: state.totalItems - 1,
+        totalPrice: state.totalPrice - food.value,
+      }));
+    } else {
+      return;
+    }
+  },
+}));
