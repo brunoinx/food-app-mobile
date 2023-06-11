030209df39ca684a3d4e60859d4f4f7ca399a97a
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-06-10 23:58:46 -0300

    refactor: :lipstick: style change bottom tab

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -48,12 +48,13 @@ export function TabRoutes() {
         tabBarActiveTintColor: colors.main[400],
         tabBarInactiveTintColor: colors.zinc[500],
         tabBarStyle: {
-          height: 54,
-          position: 'absolute',
-          bottom: 14,
+          height: 58,
           backgroundColor: colors.zinc[300],
           elevation: 0,
-          border: 'none',
+        },
+        tabBarItemStyle: {
+          paddingTop: 8,
+          paddingBottom: 14,
         },
         tabBarIcon: ({ color, focused }) => {
           const { icon: Icon } = icons.find(
