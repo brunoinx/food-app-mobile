eef33521e82cb7b693080ae059044cb3967c8faa
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-06-11 00:01:13 -0300

    feat: :sparkles: create favorites screen

---
+++ src/screens/app/Favorites/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding-top: 28px;
+`;
+
+export const WrapperHeader = styled.View`
+  padding: 0 32px 0;
+`;
